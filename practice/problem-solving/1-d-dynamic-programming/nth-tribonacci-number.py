d24106ce0e811b24d5a44b075106f3d236d90248
Shantanu Mahale <shantanu.mahale@clear.in>
2026-03-03 17:39:17 +0530

    wip

--- practice/problem-solving/1-d-dynamic-programming/nth-tribonacci-number.py
+++ practice/problem-solving/1-d-dynamic-programming/nth-tribonacci-number.py
@@ -30,3 +30,34 @@ class Solution:
         
         self.dp[n] = self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
         return self.dp[n]
+    
+class Solution:
+    # 3
+    # Dynamic Programming: Bottom-Up
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    def tribonacci (self, n: int) -> int:
+        if (n <= 2):
+            return 1 if n != 0 else 0
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 1
+        for i in range (3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+        return dp[n]
+    
+class Solution:
+    # 4
+    # Dynamic Programming: Space Optimized
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    def tribonacci (self, n: int) -> int:
+        t = [0, 1, 1]
+        if (n < 3):
+            return t[n]
+        for i in range (3, n + 1):
+            t[i % 3] = sum(t)
+        return t[n % 3]
+
+
+
