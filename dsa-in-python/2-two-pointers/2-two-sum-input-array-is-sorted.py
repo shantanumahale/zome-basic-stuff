afa09eb8707ea8fc9fdd4137be0384db4d700090
Shantanu Mahale <shantanu.mahale@clear.in>
2026-05-04 00:44:40 +0530

    Two Sum Input Array Is Sorted

---
+++ dsa-in-python/2-two-pointers/2-two-sum-input-array-is-sorted.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s > target:
+                r = r - 1
+            elif s < target:
+                l = l + 1
+            else:
+                return [(l + 1), (r + 1)]
+        
+        return [(l + 1), (r + 1)]
+                
