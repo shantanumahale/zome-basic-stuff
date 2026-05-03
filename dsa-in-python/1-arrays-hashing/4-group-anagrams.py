4142477804540faff9f0c02eb3f4f8988980e5d7
Shantanu Mahale <shantanu.mahale@clear.in>
2026-05-03 23:11:23 +0530

    Group Anagram

---
+++ dsa-in-python/1-arrays-hashing/4-group-anagrams.py
@@ -0,0 +1,17 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list) # mapping charCount to list of Anagrams
+        
+        for s in strs:
+            count = [0] * 26 # a...z
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            
+            res[tuple(count)].append(s)
+
+        return list(res.values())
+        
