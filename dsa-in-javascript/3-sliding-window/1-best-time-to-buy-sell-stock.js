f0656c58943607c861bcf69854e5acce2f48ad3d
Shantanu Mahale <shantanu.mahale@clear.in>
2026-06-21 15:10:10 +0530

    Best Time To Buy Sell Stock

---
+++ dsa-in-javascript/3-sliding-window/1-best-time-to-buy-sell-stock.js
@@ -0,0 +1,18 @@
+class Stock {
+    maxProfit (prices) {
+        let l = 0;
+        let r = 1;
+        let maxP = 0;
+
+        while (r < prices.length) {
+            if (prices[l] < prices[r]) {
+                let profit = prices[r] - prices[l];
+                maxP = Math.max(maxP, profit);
+            } else {
+                l = r;
+            }
+            r++;
+        }
+        return maxP;
+    }
+}
