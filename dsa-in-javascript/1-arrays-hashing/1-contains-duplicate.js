c861829c7aa12e08ef28217550c22543929184ec
Shantanu Mahale <shantanu.mahale@clear.in>
2026-06-21 14:09:34 +0530

    Contains Duplicate

---
+++ dsa-in-javascript/1-arrays-hashing/1-contains-duplicate.js
@@ -0,0 +1,12 @@
+class Solution {
+    hasDuplicate (nums) {
+        const seen = new Set();
+        for (const num of nums) {
+            if (seen.has(num)) {
+                return true;
+            }
+            seen.add(num);
+        } 
+        return false;
+    }
+}
