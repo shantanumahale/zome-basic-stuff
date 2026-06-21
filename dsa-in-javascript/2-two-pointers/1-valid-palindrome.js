d8a860240155098f28edfd1bae63b325ddcd1b26
Shantanu Mahale <shantanu.mahale@clear.in>
2026-06-21 14:52:07 +0530

    Valid Palindrome

---
+++ dsa-in-javascript/2-two-pointers/1-valid-palindrome.js
@@ -0,0 +1,30 @@
+class Solution {
+    isPalindrome(s) {
+        let l = 0;
+        let r = s.length - 1;
+
+        while (l < r) {
+            while (l < r && !this.alphaNum(s[l])) {
+                l++;
+            } 
+            while (l < r && !this.alphaNum(s[r])) {
+                r--;
+            }
+            if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    // Returns true if string is alphanumeric, otherwise false
+    alphaNum (c) {
+        return (
+            (c >= 'A' && c <= 'Z') || 
+            (c >= 'a' && c <= 'z') || 
+            (c >= '0' && c <= '9') 
+        );
+    }
+}
