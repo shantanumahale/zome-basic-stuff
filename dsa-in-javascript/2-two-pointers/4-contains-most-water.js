a4e948449ecd2fa0279effee61647598aad20b7d
Shantanu Mahale <shantanu.mahale@clear.in>
2026-06-21 15:03:12 +0530

    Contains Most Water

---
+++ dsa-in-javascript/2-two-pointers/4-contains-most-water.js
@@ -0,0 +1,18 @@
+class Solution {
+    maxArea (heights) {
+        let l = 0;
+        let r = heights.length - 1;
+        let res = 0;
+
+        while (l < r) {
+            const area = Math.min(heights[l], heights[r]) * (r - l);
+            res = Math.max(res, area);
+            if (heights[l] <= heights[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return res;
+    }
+}
