8559f805d862df065910cecddfb10014907099ae
Shantanu Mahale <shantanu.mahale@clear.in>
2026-06-21 14:47:22 +0530

    Longest Consecutive Sequence

---
+++ dsa-in-javascript/1-arrays-hashing/9-longest-consecutive-sequence.js
@@ -0,0 +1,17 @@
+class Solution {
+    longestConsecutive(nums) {
+        const numSet = new Set(nums);
+        let longest = 0;
+
+        for (let num of nums) {
+            if (!numSet.has(num - 1)) {
+                let length = 1;
+                while (numSet.has(num + length)) {
+                    length++;
+                }
+                longest = Math.max(longest, length);
+            }
+        }
+        return longest;
+    }
+}
