ca74a121d7329c2961292584ea868c534a3ab77a
Shantanu Mahale <shantanu.mahale@clear.in>
2026-06-21 15:12:50 +0530

    Longest Substring Without Repeating Characters

---
+++ dsa-in-javascript/3-sliding-window/2-longest-substring-without-repeating-characters.js
@@ -0,0 +1,17 @@
+class Solution {
+    lengthOfLongestSubstring(s) {
+        const charSet = new Set();
+        let l = 0;
+        let res = 0;
+
+        for (let r=0; r<s.length; r++) {
+            while(charSet.has(s[r])) {
+                charSet.delete(s[l]);
+                l++;
+            }
+            charSet.add(s[r]);
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
